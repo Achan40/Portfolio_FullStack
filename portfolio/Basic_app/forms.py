b6bedc0cd56b492730cbe728864ed40b5fee0d42
Aaron <51930273+Achan40@users.noreply.github.com>
2020-11-11 12:22:40 -0600

    added widgets to contact, beginning styling contact page

--- portfolio/Basic_app/forms.py
+++ portfolio/Basic_app/forms.py
@@ -4,9 +4,17 @@ from captcha.fields import CaptchaField
 
 class ContactForm(forms.ModelForm):
     captcha = CaptchaField()
+
     class Meta():
         model = Contact
         fields = ('author','email','text')
+
+    widgets = {
+        'author':forms.TextInput(attrs={'class':'form-control'}),
+        'email':forms.TextInput(attrs={'class':'form-control'}),
+        'text':forms.TextInput(attrs={'class':'form-control'}),
+    }
+    
 
 class ProjectForm(forms.ModelForm):
     class Meta():
