bad1d4969996ca15dc16d7af33f5c6f26660e300
Aaron <51930273+Achan40@users.noreply.github.com>
2020-11-15 21:30:39 -0600

    restructering project

--- portfolio/Basic_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Basic_app.models import Project,Contact
-
-# Register models so that we can see them on the admin site
-admin.site.register(Project)
-admin.site.register(Contact)
