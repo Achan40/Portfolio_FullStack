52db519f89777741549ecc9174042712ac6958df
Aaron <51930273+Achan40@users.noreply.github.com>
2020-11-16 20:49:44 -0600

    made changes to settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Extra places for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'staticfiles'),
+)
 
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR,'Basic_app/media/project_pic')
@@ -130,3 +137,4 @@ MEDIA_URL = '/media/'
 
 # redirect to homepage
 LOGIN_REDIRECT_URL = '/'
+
