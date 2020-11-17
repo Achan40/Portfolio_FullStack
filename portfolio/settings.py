0473718af4d9921b4a88cb17e0935d509f402584
Aaron <51930273+Achan40@users.noreply.github.com>
2020-11-16 22:54:02 -0600

    slight change to allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "Not a key"# This has been set in enviornment variables in heroku c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['achan-portfolio.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['achan-portfolio.herokuapp.com','127.0.0.1']
 
 
 # Application definition
