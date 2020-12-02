fe72e88ed2b36035fc8c4160828a5f8a0a972a7d
Aaron <51930273+Achan40@users.noreply.github.com>
2020-12-01 19:17:31 -0600

    changes to keys

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'Basic_app/templates/Basic_app')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # This has been set in enviornment variables in heroku console
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -145,8 +145,8 @@ STATICFILES_DIRS = (
 )
 
 # AWS for storing user uploaded media files only
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'achanportfolio'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
